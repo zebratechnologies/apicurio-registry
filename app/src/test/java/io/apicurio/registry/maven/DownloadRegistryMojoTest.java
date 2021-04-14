db9d346929eb5da7ff7f6ece266897fb52cc9a19
zenon-was-here <zenon.evans@zebra.com>
2021-04-14 11:31:55 -0500

    intermediary commit; removing and revising a bunch of stuff to try to upgrade to >11

--- app/src/test/java/io/apicurio/registry/maven/DownloadRegistryMojoTest.java
+++ app/src/test/java/io/apicurio/registry/maven/DownloadRegistryMojoTest.java
@@ -1,66 +1,66 @@
-/*
- * Copyright 2018 Confluent Inc. (adapted from their MojoTest)
- * Copyright 2020 Red Hat
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package io.apicurio.registry.maven;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Collections;
-
-import org.apache.avro.Schema;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import io.apicurio.registry.types.ArtifactType;
-import io.quarkus.test.junit.QuarkusTest;
-
-/**
- * @author Ales Justin
- */
-@QuarkusTest
-public class DownloadRegistryMojoTest extends RegistryMojoTestBase {
-    DownloadRegistryMojo mojo;
-
-    @BeforeEach
-    public void createMojo() {
-        this.mojo = new DownloadRegistryMojo();
-        this.mojo.registryUrl = "http://localhost:8081/api";
-    }
-
-    @Disabled("Doesn't work with H2 test env after code change for Spanner")
-    @Test
-    public void testDownloadIds() throws Exception {
-        String artifactId = generateArtifactId();
-
-        Schema schema = Schema.createUnion(Arrays.asList(Schema.create(Schema.Type.STRING), Schema.create(Schema.Type.NULL)));
-        client.createArtifact(artifactId, ArtifactType.AVRO, new ByteArrayInputStream(schema.toString().getBytes(StandardCharsets.UTF_8)));
-        
-        this.waitForArtifact(artifactId);
-
-        mojo.ids = Collections.singleton(artifactId);
-        mojo.artifactExtension = ".avsc";
-        mojo.outputDirectory = tempDirectory;
-        mojo.execute();
-
-        Assertions.assertTrue(new File(mojo.outputDirectory, artifactId + mojo.artifactExtension).exists());
-    }
-
-}
+///*
+// * Copyright 2018 Confluent Inc. (adapted from their MojoTest)
+// * Copyright 2020 Red Hat
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package io.apicurio.registry.maven;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.File;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Arrays;
+//import java.util.Collections;
+//
+//import org.apache.avro.Schema;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//
+//import io.apicurio.registry.types.ArtifactType;
+//import io.quarkus.test.junit.QuarkusTest;
+//
+///**
+// * @author Ales Justin
+// */
+//@QuarkusTest
+//public class DownloadRegistryMojoTest extends RegistryMojoTestBase {
+//    DownloadRegistryMojo mojo;
+//
+//    @BeforeEach
+//    public void createMojo() {
+//        this.mojo = new DownloadRegistryMojo();
+//        this.mojo.registryUrl = "http://localhost:8081/api";
+//    }
+//
+//    @Disabled("Doesn't work with H2 test env after code change for Spanner")
+//    @Test
+//    public void testDownloadIds() throws Exception {
+//        String artifactId = generateArtifactId();
+//
+//        Schema schema = Schema.createUnion(Arrays.asList(Schema.create(Schema.Type.STRING), Schema.create(Schema.Type.NULL)));
+//        client.createArtifact(artifactId, ArtifactType.AVRO, new ByteArrayInputStream(schema.toString().getBytes(StandardCharsets.UTF_8)));
+//        
+//        this.waitForArtifact(artifactId);
+//
+//        mojo.ids = Collections.singleton(artifactId);
+//        mojo.artifactExtension = ".avsc";
+//        mojo.outputDirectory = tempDirectory;
+//        mojo.execute();
+//
+//        Assertions.assertTrue(new File(mojo.outputDirectory, artifactId + mojo.artifactExtension).exists());
+//    }
+//
+//}
--- app/src/test/java/io/apicurio/registry/maven/TestUpdateRegistryMojoTest.java
+++ app/src/test/java/io/apicurio/registry/maven/TestUpdateRegistryMojoTest.java
@@ -1,91 +1,91 @@
-/*
- * Copyright 2018 Confluent Inc. (adapted from their MojoTest)
- * Copyright 2020 Red Hat
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package io.apicurio.registry.maven;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-
-import org.apache.avro.Schema;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import io.apicurio.registry.rest.beans.Rule;
-import io.apicurio.registry.types.ArtifactType;
-import io.apicurio.registry.types.RuleType;
-import io.quarkus.test.junit.QuarkusTest;
-
-/**
- * @author Ales Justin
- */
-@QuarkusTest
-public class TestUpdateRegistryMojoTest extends RegistryMojoTestBase {
-    TestUpdateRegistryMojo mojo;
-
-    @BeforeEach
-    public void createMojo() {
-        this.mojo = new TestUpdateRegistryMojo();
-        this.mojo.registryUrl = "http://localhost:8081/api";
-    }
-
-    @Disabled("Doesn't work with H2 test env after code change for Spanner")
-    @Test
-    public void testCompatibility() throws Exception {
-        String artifactId = generateArtifactId();
-
-        Schema schema = new Schema.Parser().parse("{\"namespace\": \"example.avro\"," +
-                                                  " \"type\": \"record\"," +
-                                                  " \"name\": \"user\"," +
-                                                  " \"fields\": [" +
-                                                  "     {\"name\": \"name\", \"type\": \"string\"}," +
-                                                  "     {\"name\": \"favorite_number\",  \"type\": \"int\"}" +
-                                                  " ]" +
-                                                  "}");
-        client.createArtifact(artifactId, ArtifactType.AVRO, new ByteArrayInputStream(schema.toString().getBytes(StandardCharsets.UTF_8)));
-        
-        this.waitForArtifact(artifactId);
-
-        Rule rule = new Rule();
-        rule.setType(RuleType.COMPATIBILITY);
-        rule.setConfig("BACKWARD");
-        client.createArtifactRule(artifactId, rule);
-
-        // add new field
-        Schema schema2 = new Schema.Parser().parse("{\"namespace\": \"example.avro\"," +
-                                                   " \"type\": \"record\"," +
-                                                   " \"name\": \"user\"," +
-                                                   " \"fields\": [" +
-                                                   "     {\"name\": \"name\", \"type\": \"string\"}," +
-                                                   "     {\"name\": \"favorite_number\",  \"type\": \"int\"}," +
-                                                   "     {\"name\": \"favorite_color\", \"type\": \"string\", \"default\": \"green\"}" +
-                                                   " ]" +
-                                                   "}");
-        File file = new File(tempDirectory, artifactId + ".avsc");
-        writeContent(file, schema2.toString().getBytes(StandardCharsets.UTF_8));
-
-        mojo.artifacts = Collections.singletonMap(artifactId, file);
-        mojo.artifactType = ArtifactType.AVRO;
-        mojo.execute();
-
-        Assertions.assertTrue(mojo.results.get(artifactId));
-    }
-
-}
+///*
+// * Copyright 2018 Confluent Inc. (adapted from their MojoTest)
+// * Copyright 2020 Red Hat
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package io.apicurio.registry.maven;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.File;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Collections;
+//
+//import org.apache.avro.Schema;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//
+//import io.apicurio.registry.rest.beans.Rule;
+//import io.apicurio.registry.types.ArtifactType;
+//import io.apicurio.registry.types.RuleType;
+//import io.quarkus.test.junit.QuarkusTest;
+//
+///**
+// * @author Ales Justin
+// */
+//@QuarkusTest
+//public class TestUpdateRegistryMojoTest extends RegistryMojoTestBase {
+//    TestUpdateRegistryMojo mojo;
+//
+//    @BeforeEach
+//    public void createMojo() {
+//        this.mojo = new TestUpdateRegistryMojo();
+//        this.mojo.registryUrl = "http://localhost:8081/api";
+//    }
+//
+//    @Disabled("Doesn't work with H2 test env after code change for Spanner")
+//    @Test
+//    public void testCompatibility() throws Exception {
+//        String artifactId = generateArtifactId();
+//
+//        Schema schema = new Schema.Parser().parse("{\"namespace\": \"example.avro\"," +
+//                                                  " \"type\": \"record\"," +
+//                                                  " \"name\": \"user\"," +
+//                                                  " \"fields\": [" +
+//                                                  "     {\"name\": \"name\", \"type\": \"string\"}," +
+//                                                  "     {\"name\": \"favorite_number\",  \"type\": \"int\"}" +
+//                                                  " ]" +
+//                                                  "}");
+//        client.createArtifact(artifactId, ArtifactType.AVRO, new ByteArrayInputStream(schema.toString().getBytes(StandardCharsets.UTF_8)));
+//
+//        this.waitForArtifact(artifactId);
+//
+//        Rule rule = new Rule();
+//        rule.setType(RuleType.COMPATIBILITY);
+//        rule.setConfig("BACKWARD");
+//        client.createArtifactRule(artifactId, rule);
+//
+//        // add new field
+//        Schema schema2 = new Schema.Parser().parse("{\"namespace\": \"example.avro\"," +
+//                                                   " \"type\": \"record\"," +
+//                                                   " \"name\": \"user\"," +
+//                                                   " \"fields\": [" +
+//                                                   "     {\"name\": \"name\", \"type\": \"string\"}," +
+//                                                   "     {\"name\": \"favorite_number\",  \"type\": \"int\"}," +
+//                                                   "     {\"name\": \"favorite_color\", \"type\": \"string\", \"default\": \"green\"}" +
+//                                                   " ]" +
+//                                                   "}");
+//        File file = new File(tempDirectory, artifactId + ".avsc");
+//        writeContent(file, schema2.toString().getBytes(StandardCharsets.UTF_8));
+//
+//        mojo.artifacts = Collections.singletonMap(artifactId, file);
+//        mojo.artifactType = ArtifactType.AVRO;
+//        mojo.execute();
+//
+//        Assertions.assertTrue(mojo.results.get(artifactId));
+//    }
+//
+//}
